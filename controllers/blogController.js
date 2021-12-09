01ed8804e25e23f7683764675663c57630e3c7d9
Faiyaz Rahman <faiyazrahman03@gmail.com>
2021-12-09 14:41:40 +0600

    used cloudinary for image management

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -4,11 +4,13 @@ const fs = require('fs');
 const Blog = require('../models/Blog');
 const Category = require('../models/Category');
 const catchAsync = require('../utils/catchAsync');
+const cloudinary = require('./../utils/cloudinary');
 
 const createBlog = catchAsync(async (req, res, next) => {
-   const imageAccessLink = `${process.env.APP_URL}/uploads/${req.file.filename}`;
-   const image = req.file.filename;
-   const blog = await Blog.create({ ...req.body, image: image, imageAccessLink: imageAccessLink });
+   const imageName = req.file.filename;
+   const { public_id, url } = await cloudinary.uploader.upload(req.file.path);
+
+   const blog = await Blog.create({ ...req.body, image: imageName, imageAccessLink: url, imageId: public_id });
 
    await Category.findOneAndUpdate(
       { name: blog.category },
@@ -17,7 +19,7 @@ const createBlog = catchAsync(async (req, res, next) => {
       }
    );
 
-   res.status(200).json({ status: 'sucess', message: 'Blog created successfully' });
+   res.status(200).json({ status: 'sucess', message: 'Blog created successfully', blog });
 });
 
 const getAllBlogs = catchAsync(async (req, res, next) => {
@@ -47,14 +49,16 @@ const getBlogsByCategory = catchAsync(async (req, res, next) => {
 const deleteBlog = catchAsync(async (req, res, next) => {
    const deletedBlog = await Blog.findByIdAndDelete({ _id: req.body.id });
 
+   //delete image local and from cloudinary
    const path = `${__dirname}/../public/uploads/${deletedBlog.image}`;
-
    fs.unlink(path, (err) => {
       if (err) {
          console.error(err);
       }
    });
 
+   await cloudinary.uploader.destroy(deletedBlog.imageId, { type: 'upload', resource_type: 'image' });
+   //delete the blog from the corresponding category
    await Category.findOneAndUpdate(
       { name: deletedBlog.category },
       {
