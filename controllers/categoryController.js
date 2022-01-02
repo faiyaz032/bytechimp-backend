63b83f0396e029d52073b1969b2418ff0e115ba9
Faiyaz Rahman <faiyazrahman03@gmail.com>
2022-01-02 23:55:48 +0600

    added results on category field

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -5,7 +5,12 @@ const catchAsync = require('../utils/catchAsync');
 
 const getCategories = catchAsync(async (req, res, next) => {
    const categories = await Category.find({}).select({ __v: 0 });
-   res.status(200).json({ status: 'success', message: 'Categories fetched successfully', categories });
+   res.status(200).json({
+      status: 'success',
+      message: 'Categories fetched successfully',
+      results: categories.length,
+      categories,
+   });
 });
 const createCategory = catchAsync(async (req, res, next) => {
    await Category.create({ ...req.body });
